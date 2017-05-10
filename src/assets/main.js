14e6274f624f84c73ea4b63b93e3abd4b87030e6
alex-sanders <asanders.impaqt@gmail.com>
2017-05-10 18:00:19 -0400

    challenge 13
    13

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,15 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer.value == "" || attempt.value == ""){
+      setHiddenFields();
+    }
+
+    if (validateInput(input.value) !== true){
+      return;
+    } else {
+      attempt.value++;
+    }
 }
 
 //implement new functions here
@@ -12,6 +21,31 @@ function setHiddenFields(){
   while(answer.value.length < 4) {
       answer.value = "0" + answer.value;
   }
-  return answer;
+  attempt.value = 0;
+}
 
+function setMessage(message){
+  get.getElementById('message').innerHTML = message;
+}
+
+function validateInput(input) {
+  if(input.length == 4) {
+    return true;
+  } else {
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  }
+}
+
+function getResults(input){
+  let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+  for(i=0; i<input.length ;i++){
+    if(input.charAt(i) == answer.value.charAt(i)){
+        html+='<span class="glyphicon glyphicon-ok"></span>';
+      } else if (answer.value.indexOf(input.charAt(i) > -1){
+          html+='<span class="glyphicon glyphicon-transfer"></span>';
+        } else {
+            html+='<span class="glyphicon glyphicon-remove"></span>';
+          }
+  }
 }
