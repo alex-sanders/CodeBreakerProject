23882ab2d45e3d4ba846e70c88ea3aa4870698cf
alex-sanders <asanders.impaqt@gmail.com>
2017-05-10 17:34:52 -0400

    Challenges 5-7

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,11 @@ function guess() {
 }
 
 //implement new functions here
+function setHiddenFields(){
+  answer.value = math.floor(math.random() * 10000).toString();
+  while(answer.value.length < 4) {
+      answer.value = "0" + answer.value;
+  }
+  return answer;
+
+}
